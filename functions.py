e72dcddf482fb6262e0321954133de5de390129b
prestonchee <prestonchee1@gmail.com>
2022-11-15 19:07:25 -0700

    calc for in to mm

--- functions.py
+++ functions.py
@@ -6,5 +6,20 @@
 #convert unit to the correct unit
 #output the answer to the user
 
-user_number = input ("What number would you like too convert? ")
+user_number = float(input ("What number would you like too convert? "))
 user_unit = input ("What unit is your number? ")
+
+#convert inches to mm --> in X 25.4
+#to convert mm to in --> mm / 25.4
+
+#user gives in unit
+
+conv_mm = user_number * 25.4
+
+# user gies mm unit
+
+conv_in = user_number / 25.4
+
+
+print (conv_mm)
+print (user_number)

b009d59e303c6ec87257f43d7174a8c9b569930c
prestonchee <prestonchee1@gmail.com>
2022-11-22 17:42:25 -0700

    update code

--- functions.py
+++ functions.py
@@ -1,26 +1,39 @@
-# conversion calculator
-#by:
+# Conversion Calculator
+# By: Scott Hadzik
 
 # user input regarding the length to convert
 # get the unit from the user
-#convert unit to the correct unit
-#output the answer to the user
+# convert the length to the correct unit
+# output the answer to the user
+def user_parser(user_input):
+# Do something
+# Separate the number from unit
+    values = user_input.rsplit(" ")
+    print (values)
 
-user_number = float(input ("What number would you like too convert? "))
-user_unit = input ("What unit is your number? ")
+while True:
+    user_input = input("number and unit to convert ")
+    user_parser(user_input)
+# while True:
+# user_number = input("What number to convert? ")
+# if user_number.isdigit():
+# user_number = float(user_number)
+# break
+# else:
+# print ('please use a number')
+# user_unit = input("What unit is your number?")
 
-#convert inches to mm --> in X 25.4
-#to convert mm to in --> mm / 25.4
+# if(user_unit == 'in'):
+# #perform in to mm
+# conv_number = user_number * 25.4
+# conv_unit = 'mm'
+# break
+# elif(user_unit == 'mm'):
+# #perform mm to in
+# conv_number = user_number / 25.4
+# conv_unit = 'in'
+# break
+# else:
+# print('That is not a valid unit')
 
-#user gives in unit
-if (user_unit =='in'):
-    conv_num = user_number * 25.4
-    conv_unit = 'mm'
-elif (user_unit == 'mm'):
-# user gies mm unit
-    conv_num = user_number / 25.4
-    conv_unit = 'in'
-else:
-    print('That is not a valid unit')
-    
-print (conv_num, conv_unit)
+# print(conv_number, conv_unit)
